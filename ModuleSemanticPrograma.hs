be8f926492e08fc641ba1c45111693adb2c20495
Wallace Mielniczki <wmielniczki15@gmail.com>
2023-11-13 16:51:56 -0300

    Delete ModuleSemanticPrograma.hs

--- ModuleSemanticPrograma.hs
+++
@@ -1,8 +0,0 @@
-module ModuleSemanticPrograma where
-
-import ModuleDataTypes
-import ModuleLexico
-import ModuleFuncao
-import ModuleVar
-
-verificaPrograma cabecalho funcoes variaveis bloco = (TInt, "teste", TDouble)
